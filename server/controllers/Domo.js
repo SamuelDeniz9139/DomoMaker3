8087a1dce9314273b25dc43a15c70599eeb94743
Sam <sfd9139@g.rit.edu>
2022-04-02 16:14:05 -0400

    trying to fix an error

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -3,6 +3,7 @@ const Domo=models.Domo;
 const makerPage=(req,res)=>{
     Domo.findByOwner(req.session.account._id,(err,docs)=>{
         if(err){
+            console.log(err);
             return res.status(400).json({error:'An error occurred.'});
         }
         return res.render('app',{csrfToken:req.csrfToken(),domos:docs});
